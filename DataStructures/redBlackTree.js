ffc364ebe898813d431657b654759a19cbb0473e
Tyler Becks <tylerdbecks@gmail.com>
2016-09-20 16:48:31 -0700

    Add redblacktree put method

--- DataStructures/redBlackTree.js
+++ DataStructures/redBlackTree.js
@@ -7,17 +7,26 @@ class Node {
   }
 }
 
-class leftLeaningRedBlackTree {
+class LeftLeaningRedBlackTree {
   constructor() {
     this.root = null;
   }
 
-  insert(val) {
+  put(val, node = this.root) {
     if (this.root === null) {
       this.root = new Node(val);
+      this.root.isRed = false;
+      node = this.root;
+    } else if (node === null) {
+      return new Node(val);
+    } else if (val < node.val) {
+      node.left = this.put(val, node.left);
     } else {
-      
+      node.right = this.put(val, node.right);
     }
+
+    node = this.balanceCheck(node);
+    return node;
   }
 
   search(val) {
@@ -33,29 +42,60 @@ class leftLeaningRedBlackTree {
     }
   }
 
-  rotateLeft(node, parent) {
+  balanceCheck(node) {
+    if (node.left && node.left.left && node.left.isRed && node.left.left.isRed) { node = this.rotateRight(node); }
+    if (node.left && node.right && node.left.isRed && node.right.isRed) { this.swapColors(node); }
+    if (node.right && node.right.isRed) { node = this.rotateLeft(node); }
+    return node;
+  }
+
+  rotateLeft(node) {
+    console.log(`left rotating ${node.val}, isRed: ${node.isRed}`);
     let newTop = node.right;
     let nodeIsRed = node.isRed;
     node.right = newTop.left;
     node.isRed = newTop.isRed;
     newTop.left = node;
-    newTop.isRed = node.isRed;
-    node === parent.left ? parent.left = newtop : parent.right = newTop;
+    newTop.isRed = nodeIsRed;
+    if (node === this.root) { this.root = newTop; }
+    return newTop;
   }
 
-  rotateRight(node, parent) {
+  rotateRight(node) {
+    console.log(`right rotating ${node.val}, isRed: ${node.isRed}`);
     let newTop = node.left;
     let nodeIsRed = node.isRed;
     node.left = newTop.right;
-    newTop.right = null;
     node.isRed = newTop.isred;
+    newTop.right = node;
     newTop.isRed = nodeIsRed;
-    node === parent.left ? parent.left = newtop : parent.right = newTop;
+    if (node === this.root) { this.root = newTop; }
+    return newTop;
   }
 
   swapColors(node) {
-    node.isRed = !node.isRed;
+    console.log(`swapping colors on ${node.val}`);
+    if (node !== this.root) { node.isRed = !node.isRed; }
     node.left.isRed = !node.left.isRed;
     node.right.isRed = !node.right.isRed;
   }
+
+  breadthFirst(callback) {
+    let queue = [this.root];
+    while (queue.length > 0) {
+      if (queue[0].left) { queue.push(queue[0].left); }
+      if (queue[0].right) { queue.push(queue[0].right); }
+      callback(queue.shift());
+    }
+  }
 }
+
+// error with the isRed property on 6 disapearing
+const tree = new LeftLeaningRedBlackTree();
+tree.put(5);
+tree.put(7);
+tree.put(10);
+tree.put(6);
+tree.put(4);
+tree.breadthFirst(item => console.log(item.val, item.isRed));
+console.log(JSON.stringify(tree));
