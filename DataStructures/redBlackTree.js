0810ea301789a2054008325f534a0f6bb8b4b0fe
Tyler Becks <tylerdbecks@gmail.com>
2016-09-20 11:15:56 -0700

    add rotations to redblacktree

--- DataStructures/redBlackTree.js
+++ DataStructures/redBlackTree.js
@@ -1,10 +1,23 @@
-class redBlackTree {
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.isRed = true;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class leftLeaningRedBlackTree {
   constructor() {
     this.root = null;
   }
 
   insert(val) {
-
+    if (this.root === null) {
+      this.root = new Node(val);
+    } else {
+      
+    }
   }
 
   search(val) {
@@ -20,15 +33,29 @@ class redBlackTree {
     }
   }
 
-  rotateLeft(node) {
-
+  rotateLeft(node, parent) {
+    let newTop = node.right;
+    let nodeIsRed = node.isRed;
+    node.right = newTop.left;
+    node.isRed = newTop.isRed;
+    newTop.left = node;
+    newTop.isRed = node.isRed;
+    node === parent.left ? parent.left = newtop : parent.right = newTop;
   }
 
-  rotateRight(node) {
-
+  rotateRight(node, parent) {
+    let newTop = node.left;
+    let nodeIsRed = node.isRed;
+    node.left = newTop.right;
+    newTop.right = null;
+    node.isRed = newTop.isred;
+    newTop.isRed = nodeIsRed;
+    node === parent.left ? parent.left = newtop : parent.right = newTop;
   }
 
   swapColors(node) {
-
+    node.isRed = !node.isRed;
+    node.left.isRed = !node.left.isRed;
+    node.right.isRed = !node.right.isRed;
   }
 }
